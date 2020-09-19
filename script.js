477dbfb91bd86039c4a1a241a7ad84ca7ebd53ea
Anushka Raj <anush6633@gmail.com>
2020-09-19 14:36:09 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+anime({
+    targets: 'h1',
+    rotate:'1turn',
+    duration: 3000,
+    loop: true
+  });
+
+anime({
+    targets: 'li',
+    translateX:-20,
+    duration: 3000,
+    rotate:'1turn',
+  });
